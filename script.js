d9544e4c085e7a889857339d3fcadb529eca0899
ALLOMOH <105246117+ALLOMOH@users.noreply.github.com>
2024-03-16 15:58:56 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const progressBar = document.querySelector('.progressbar');
+
+let progress = 0 ;
+function enableProgressbar(){
+    progressBar.setAttribute("role","progressbar");
+    progressBar.setAttribute("aria-valuenow",progress);
+    progressBar.setAttribute("aria-live","polite");
+}
+
+enableProgressbar();
+// for simulation
+
+const testingGround = document.querySelector('.testing-ground');
+let interval = null;
+
+function simulateProgress(newProgressValue){
+    clearInterval(interval);
+    if(newProgressValue === "fake-upload"){
+        simulateUpload();
+    }else{
+        updateProgress(newProgressValue);
+    }
+}
+
+testingGround.addEventListener("click",(e)=>{
+    if(!e.target.closest("button")) return;
+    progress = e.target.dataset.progress;
+    simulateProgress(progress);
+});
+
+function updateProgress(progress){
+    progressBar.setAttribute("aria-valuenow",progress);
+    progressBar.style.setProperty('--progress',progress + "%");
+}
+
+function simulateUpload(){
+    progressBar.setAttribute("aria-busy","true");
+    let progress = 0;
+    updateProgress(progress);
+
+    intervalTimer = setInterval(()=>{
+        progress += 5;
+        updateProgress(progress);
+        if(progress === 100){
+            progressBar.setAttribute("aria-busy","false");
+            clearInterval(intervalTimer);
+        }
+    }, 500);
+}
